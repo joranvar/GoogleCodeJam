506e92ca8ddeb746069b8391f2408e19ae31655c
joranvar <bart.post@gmail.com>
2014-05-12 14:39:08 +0200

    Made the Y2014.R1C.B solution a bit smarter/faster

--- Y2014/R1C/B.hs
+++ Y2014/R1C/B.hs
@@ -1,7 +1,8 @@
 module Y2014.R1C.B where
 
-import Control.Monad
 import Data.List
+import Data.List.Utils
+import Data.Maybe
 import qualified Data.Set as Set
 
 solve :: Problem -> Solution
@@ -18,28 +19,45 @@ data Problem = Problem [String] deriving (Eq,Show)
 parse [] = []
 parse (_:cars:rest) = Problem (words cars) : parse rest
 
-solve (Problem cars)
-    = Solution $ fromIntegral $ length $ allCombos $ map reducedCar cars
+-- Cars with the same letter have to be combined each other: less mobile
+-- For any letter, there can only be two cars with also another letter
+-- Cars with unique letters can be put anywhere left
 
-carMiddle :: String -> String
-carMiddle [] = []
-carMiddle (_:[]) = []
-carMiddle (_:_:[]) = []
-carMiddle xs = tail.init $ xs
+solve (Problem c)
+    -- Any single offending car is invalid
+    | any (not . validCombo) cars = Solution 0
+    -- Any two equal cars with more than one letter (-kind) is invalid; needed for sets
+    | any (\grp -> length grp > 1) $ group [car | car <- cars, length car > 1] = Solution 0
+    -- Otherwise, count pure equal cars (one letter) as permutations and permutate the set
+    | otherwise = let pureCarsPossibilitiesCount = product $ map (factorial . length) ([]:group pureCars)
+                      factorial l                = product (take (l+1) (1:[1..]))
+                      pureCars                   = [car | car <- cars, length car == 1]
+                      set                        = uniq cars
+                  in case allCombos set of
+                       Nothing -> Solution 0
+                       Just xs -> Solution (pureCarsPossibilitiesCount * fromIntegral (length xs))
+    where cars = sort $ map reducedCar c
 
-reducedCar :: String -> String
+reducedCar     :: String -> String
 reducedCar car = map head (group car)
 
-allCombos :: [String] -> [String]
-allCombos xs = concatMap allCombos' [x:(xs\\[x]) | x <- xs]
+allCombos    :: [String] -> Maybe [String]
+allCombos xs = case mapM allCombos' [x:(xs\\[x]) | x <- xs] of
+                 Nothing -> Nothing
+                 Just a  -> Just $ concat a
 
-allCombos' :: [String] -> [String]
+allCombos'         :: [String] -> Maybe [String]
 allCombos' (x:[])
-    | validCombo x = [x]
-    | otherwise    = []
-allCombos' (x:ys) = concatMap allCombos' [(x++y):(ys\\[y]) | y <- ys, validCombo (x++y)]
+    | validCombo x = Just [x]
+    | otherwise    = Nothing
+allCombos' (x:ys)  = case map allCombos' [(x++y):(ys\\[y]) | y <- ys, validCombo (x++y)] of
+                       xs
+                           | any isNothing xs -> Nothing
+                           | otherwise        -> case sequence xs of
+                                                   Nothing  -> Nothing
+                                                   Just mxs -> Just (concat mxs)
 
-validCombo :: String -> Bool
+validCombo    :: String -> Bool
 validCombo xs = length unique == Set.size set
    where unique = map head $ group xs
-         set = Set.fromList unique
+         set    = Set.fromList unique
--- Y2014/R1C/BTest.hs
+++ Y2014/R1C/BTest.hs
@@ -28,5 +28,35 @@ test_solve2 = assertEqual (Solution 4) $ solve $ Problem ["aa","aa","bc","c"]
 test_solve3 :: IO ()
 test_solve3 = assertEqual (Solution 0) $ solve $ Problem ["abc","bcd"]
 
+test_singleCar_Solution1 =
+    assertEqual (Solution 1) $ solve $ Problem ["abc"]
+
+test_singlePureCar_Solution1 =
+    assertEqual (Solution 1) $ solve $ Problem ["aaa"]
+
+test_doublePureCar_Solution2 =
+    assertEqual (Solution 2) $ solve $ Problem ["aaa", "a"]
+
+test_doubleCar_Solution2 =
+    assertEqual (Solution 2) $ solve $ Problem ["aaa", "bcd"]
+
+test_triplePureCar_Solution6 =
+    assertEqual (Solution 6) $ solve $ Problem ["aaa", "aaa", "aaa"]
+
+test_threePureCarOneDifferent_Solution4 =
+    assertEqual (Solution 4) $ solve $ Problem ["aaa", "a", "bbbb"]
+
+test_doubleDoublePureCar_Solution8 =
+    assertEqual (Solution 8) $ solve $ Problem ["aa","aaaa","vv","vvvvv"]
+
+test_doubleDoublePureCarPlusBinder_Solution4 =
+    assertEqual (Solution 4) $ solve $ Problem ["aa","aaaa","vv","vvvvv", "av"]
+
+test_doubleDoublePureCarPlusPrefixBinder_Solution8 =
+    assertEqual (Solution 8) $ solve $ Problem ["aa","aaaa","vv","vvvvv", "ag"]
+
+test_twoIdenticallyBoundCars_Solution0 =
+    assertEqual (Solution 0) $ solve $ Problem ["ab","ab"]
+
 test_singleInvalidCar_Solution0 =
     assertEqual (Solution 0) $ solve $ Problem ["cac"]
