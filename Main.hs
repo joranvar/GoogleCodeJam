7b0c95a6ec9a7937a8f28887997451b4e47ab463
joranvar <bart.post@gmail.com>
2015-04-06 20:14:28 +0200

    Add parallelism

--- Main.hs
+++ Main.hs
@@ -1,10 +1,11 @@
 module Main where
 
+import Control.Parallel.Strategies
 import Y2010.Q.C (solve, parse)
 
 main :: IO ()
 main = getContents >>=
-       putStr . unlines . showSolutions . map (show . solve) . parse . tail . lines
+       putStr . unlines . showSolutions . parMap rdeepseq (show . solve) . parse . tail . lines
 
 showSolutions :: [String] -> [String]
 showSolutions = zipWith (++) ["Case #" ++ show i ++ ": " | i <- [1::Int ..]]
