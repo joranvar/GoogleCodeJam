4066e662edf55fd8fb8768b0a110cad48c4d9b8b
joranvar <bart.post@gmail.com>
2015-04-08 06:07:28 +0200

    Naive placeholder solution

--- Y2010/R1A/A.hs
+++ Y2010/R1A/A.hs
@@ -6,8 +6,9 @@ import Data.Maybe
 solve :: Problem -> Solution
 parse :: [String] -> [Problem]
 
-data Solution = Neither | Both | Red | Blue
-                deriving (Eq, Show)
+data Solution = Solution { red :: Bool
+                         , blue :: Bool
+                         } deriving (Eq, Show)
 
 data Piece = R | B | N
            deriving (Eq, Show)
@@ -24,4 +25,17 @@ parse (dimensions:rest) =
           readPiece 'B' = B
 parse [] = []
 
-solve (Problem _board _k) = Neither
+solve (Problem _board _k)
+    | count B < _k && count R < _k = Solution False False
+    | count B < _k = Solution (checkWin R) False
+    | count R < _k = Solution False (checkWin B)
+    | otherwise = Solution (checkWin R) (checkWin B)
+    where count x = sum $ map (\i -> if i == x then 1 else 0) $ concat _board
+          shrunkBoard = dropWhile (all (== N)) _board
+          tiltedBoard = map (reverse . filter (not . (==) N)) shrunkBoard
+          checkWin x = horizCheckWin x tiltedBoard || vertCheckWin x tiltedBoard || diagACheckWin x tiltedBoard || diagBCheckWin x tiltedBoard
+          horizCheckWin x = any (horizWin x)
+          horizWin x line = any (\l -> length l >= _k && head l == x) $ group line
+          vertCheckWin x b = horizCheckWin x $ transpose b
+          diagACheckWin x b = False
+          diagBCheckWin x b = diagBCheckWin x $ transpose b
--- Y2010/R1A/ATest.hs
+++ Y2010/R1A/ATest.hs
@@ -12,18 +12,41 @@ test_parse = assertEqual (Problem [[R,N,N,N],
              $ head $ parse
              ["4 4", "R...", "BR..", "BR..", "BR.."]
 
--- example0 = "4 6 4\n\
--- \1 4 2 1"
--- example1 = "100 10 1\n\
--- \1"
--- example2 = "5 5 10\n\
--- \2 4 2 3 4 2 1 2 1 3"
+example0 = "7 3\n\
+\.......\n\
+\.......\n\
+\.......\n\
+\...R...\n\
+\...BB..\n\
+\..BRB..\n\
+\.RRBR.."
 
--- test_example0 :: IO ()
--- test_example0 = assertEqual (Solution 21) $ solve . head . parse . lines $ example0
--- test_example1 :: IO ()
--- test_example1 = assertEqual (Solution 100) $ solve . head . parse . lines $ example1
--- test_example2 :: IO ()
--- test_example2 = assertEqual (Solution 20) $ solve . head . parse . lines $ example2
+example1 = "6 4\n\
+\......\n\
+\......\n\
+\.R...R\n\
+\.R..BB\n\
+\.R.RBR\n\
+\RB.BBB"
+
+example2 = "4 4\n\
+\R...\n\
+\BR..\n\
+\BR..\n\
+\BR.."
+
+example3 = "3 3\n\
+\B..\n\
+\RB.\n\
+\RB."
+
+test_example0 :: IO ()
+test_example0 = assertEqual (Solution False False) $ solve . head . parse . lines $ example0
+test_example1 :: IO ()
+test_example1 = assertEqual (Solution True True) $ solve . head . parse . lines $ example1
+test_example2 :: IO ()
+test_example2 = assertEqual (Solution True False) $ solve . head . parse . lines $ example2
+test_example3 :: IO ()
+test_example3 = assertEqual (Solution False True) $ solve . head . parse . lines $ example3
 
 {-# ANN module ("hlint:ignore Use camelCase"::String) #-}
